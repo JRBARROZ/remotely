98c4a249f931b130fb518e9d1e82826e79ef4c4b
bertonnipaz <bertonnipaz@gmail.com>
2021-05-27 22:30:28 -0300

    #7 Adding the frontend structure

---
+++ frontend/src/components/AddUser.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="flex flex-col items-center justify-center mt-7">
+    <h1>Add user</h1>
+    <form action="#" class="flex flex-col gap-2" autocomplete="off">
+      <label for="name">Name: *</label>
+      <input type="text" class="focus:outline-none border-b-2 border-black" 
+        id="name" v-model="currentUser.name" />
+      <label for="email">E-mail: *</label>
+      <input type="text" class="focus:outline-none border-b-2 border-black" 
+        id="email" v-model="currentUser.email" />
+      <label for="password">Password: *</label>
+      <input type="password" class="focus:outline-none border-b-2 border-black" 
+        id="password" v-model="currentUser.password" />
+      <button class="mt-3 py-2 bg-green-600 text-white focus:outline-none 
+      border-none rounded" type="submit" @click.prevent="handleSubmit()">Add</button>
+    </form>
+    <span class="mt-6">
+      {{ addStatus }}
+    </span>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  data(){
+    return {
+      currentUser: {
+        name: '',
+        email: '',
+        password: '',
+      }
+    }
+  },
+  name: 'AddUser',
+  computed: {
+    ...mapState(['addStatus'])
+  },
+  methods: {
+    handleSubmit() {
+      if (this.currentUser.name.trim() === '' || this.currentUser.email.trim() === '' || 
+        this.currentUser.password.trim() === '') return alert('all fields must be filled in');
+      this.$store.dispatch('addUser', this.currentUser);
+    }
+  },
+}
+</script>
---
+++ frontend/src/store/index.js
@@ -0,0 +1,66 @@
+import { createStore } from 'vuex'
+const server = 'http://localhost:8000/api';
+const axios = require('axios').default;
+
+export default createStore({
+
+  state: {
+    allUsers: [],
+    user: {},
+    addStatus: '',
+    removeStatus: '',
+  },
+  mutations: {
+    setAllUsers(state, value) {
+      state.allUsers = [...value];
+    },
+    setRemoveUser(state, index) {
+      state.allUsers.splice(index, 1);
+    },
+    setAddStatus(state, value) {
+      state.addStatus = value;
+    },
+    setRemoveStatus(state, value) {
+      state.removeStatus = value;
+    }
+  },
+  actions: {
+    async addUser({commit, context}, data) {
+      try {
+        commit('setAddStatus', 'Please wait...');
+        const response = await axios.post(`${server}/users`, data);
+        if(response.status === 200) commit('setAddStatus', 'User successfully added!');
+      } catch(error) {
+        console.log(error);
+        commit('setAddStatus', `Error while trying to add: Error: ${error.message}`);
+      }
+    },
+    async getUsers({commit}) {
+      try {
+        const response = await axios.get(`${server}/users`);
+        commit('setAllUsers', response.data);
+      } catch(error) {
+        console.log(error);
+      }
+    },
+    async updateUser(data) {
+      await axios.patch(`${server}/users/${data.id}`, data);
+    },
+    async removeUser({commit, context}, data) {
+      try {
+        const id = data[0];
+        const idx = data[1];
+        commit('setRemoveStatus', 'Please wait...');
+        await axios.delete(`${server}/users/${id}`);
+        commit('setRemoveStatus', 'User successfully deleted!');
+        commit('setRemoveUser', idx);
+      } catch(error) {
+        console.log(`%c${error}`, 'color: green');
+        commit('setRemoveStatus', `Error while trying to remove: Error: ${error.message}`);
+      } finally {
+      }
+    }
+  },
+  modules: {
+  }
+})
