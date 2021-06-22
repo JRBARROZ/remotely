8d8c6228c86b7b4765ad5b344c6a08c23ba96219
bertonnipaz <bertonnipaz@gmail.com>
2021-06-22 15:48:15 -0300

    Removing unecessary method

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <div class="flex justify-center items-center gap-2">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/register">Add User</router-link> |
+    <div class="flex justify-center items-center gap-4 my-3">
+      <router-link to="/">Home</router-link>
+      <router-link to="/register" v-if="this.token.length === 0">Add User</router-link>
       <button class="focus:outline-none" v-if="this.token.length > 0" @click="logout">Logout</button>
       <router-link to="/profile" v-if="this.token.length > 0">Profile</router-link>
       <router-link v-else to="/login">Login</router-link>
@@ -13,22 +13,8 @@
 
 <script>
 import { mapState } from 'vuex';
-const axios = require('axios').default;
 
 export default {
-  // created() {
-  //   axios.interceptors.response.use(undefined, (error) => {
-  //     return new Promise((resolve, reject) => {
-  //       if (error.status === 401 && error.config && !error.config.__isRetryRequest) {
-  //         this.$store.dispatch('auth/logout');  
-  //       }
-  //       throw error;
-  //     })
-  //   })
-  // },
-  updated() {
-
-  },
   computed: {
     ...mapState('auth', { token: state => state.token})
   },
