43be628a8dc800b63f1010a413e7cb76d07f948c
bertonnipaz <bertonnipaz@gmail.com>
2021-07-24 16:21:38 -0300

    Including some icons and colors from wireframe

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,16 @@
 module.exports = {
+  mode: 'jit',
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'input': '#E2E2FF',
+        'primary': '#8988FD',
+        'input-text': '#A8A8D3',
+        'title': '#58588B',
+      }
+    },
   },
   variants: {
     extend: {},
